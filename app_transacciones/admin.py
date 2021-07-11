641fabb6671a68b072370c0e9d9afbf248c7c37b
JosueAbisaiLopez0930 <lopez.josue201590@gmail.com>
2021-07-10 21:22:10 -0600

    primer commit

--- app_transacciones/admin.py
+++ app_transacciones/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Conferencia, Conferencista, Participante, Asistencia
 
-# Register your models here.
+# ------------------------------------------------------------------------
+class ConferenciaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'fecha', 'hora', 'estado',)
+    list_editable = ('nombre', 'fecha', 'estado',)
+
+admin.site.register(Conferencia, ConferenciaAdmin)
+# ------------------------------------------------------------------------
+admin.site.register(Conferencista)
+# ------------------------------------------------------------------------
+admin.site.register(Asistencia)
+# ------------------------------------------------------------------------
+admin.site.register(Participante)
+# ------------------------------------------------------------------------
+
--- app_transacciones/models.py
+++ app_transacciones/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=150)
+    fecha_nacimiento = models.DateField()
+    telefono = models.CharField(max_length=15)
+    correo = models.EmailField(max_length=254)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+
+class CuentaB(models.Model):
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=30, null=True, blank=True)
+    saldo = models.DecimalField(max_digits=10, decimal_places=2)
+    propietario_cuenta = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+class Deposito(models.Model):
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    propietario_cuenta = models.ForeignKey(CuentaB, on_delete=models.CASCADE)
+
+class Transaccion(models.Model):
+
+    fecha_Transaccion = models.DateTimeField(auto_now_add=True)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    propietario_cuenta = models.ForeignKey(CuentaB, on_delete=models.CASCADE)
+    nombre = models.ForeignKey(Cliente, on_delete=models.CASCADE, null=True, blank=True)
+
+class Retiro(models.Model):
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    propietario_cuenta = models.ForeignKey(CuentaB, on_delete=models.CASCADE)
